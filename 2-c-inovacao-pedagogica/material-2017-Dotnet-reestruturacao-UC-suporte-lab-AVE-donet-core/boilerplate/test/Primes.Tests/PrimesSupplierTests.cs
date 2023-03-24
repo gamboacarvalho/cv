3ef3cda734688d6a6d2cd73c4a31fe6383d65e43
Miguel Gamboa <miguelgamboa@outlook.com>
2023-03-24 13:00:19 +0000

    Moving AVE from .Net to new Dotnet core. This project is a boilerplate for Visual Studio Code C# solution .Net Core with unit tests.

---
+++ 2-c-inovacao-pedagogica/material-2017-Dotnet-reestruturacao-UC-suporte-lab-AVE-donet-core/boilerplate/test/Primes.Tests/PrimesSupplierTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Primes.Tests
+{
+    public class PrimesSupplierTests
+    {
+        [Fact]
+        public void StreamTest() 
+        {
+            int[] expected = {2, 3, 5, 7, 11, 13, 17};
+            Assert.Equal(expected, PrimesSupplier.Stream().Take(expected.Length));
+        }
+
+        [Fact]
+        public void FuncTest() 
+        {
+            int[] expected1 = {2, 3, 5, 7, 11, 13, 17};
+            Assert.Equal(expected1, PrimesSupplier.Stream().Take(expected1.Length));
+            int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+            var primes = ToIterator(PrimesSupplier.Func()).Take(expected.Length).ToArray();
+            Assert.Equal(expected, primes);
+        }
+
+        private static IEnumerable<T> ToIterator<T>(Func<T> f)
+        {
+            while(true) yield return f();
+        }
+
+    }
+}
